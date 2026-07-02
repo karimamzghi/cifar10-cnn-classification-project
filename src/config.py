0f77134f14d5c3d3c1bc35aac46ce41b18dcd049
karimamzghi <karima.mzoughi@zeynlo.com>
2026-07-02 14:23:34 +0200

    update config file

--- src/config.py
+++ src/config.py
@@ -1,6 +1,17 @@
+# Dataset configuration
 NUM_CLASSES = 10
 INPUT_SHAPE = (32, 32, 3)
 
+# Compiling configuration
 ADAM_OP = "adam"
 CATEGORICAL_CROSSENTROPY = "categorical_crossentropy"
 ACCURACY = "accuracy"
+
+# Training configuration
+BATCH_SIZE = 64
+EPOCHS = 20
+LEARNING_RATE = 0.001
+
+# Paths
+RESULTS_PATH = "results/model_tracking.csv"
+MODELS_DIR = "models"
