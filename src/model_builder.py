411870fa5f465d649b23e2bfa23c3fb685da0e92
karimamzghi <karima.mzoughi@zeynlo.com>
2026-07-03 06:05:07 +0200

    implement Barch Optimization model

--- src/model_builder.py
+++ src/model_builder.py
@@ -31,3 +31,34 @@ def build_dropout_cnn():
     )
 
     return model
+
+def build_batchnorm_cnn():
+
+    model = keras.Sequential(
+        [
+            keras.Input(shape=config.INPUT_SHAPE),
+
+            layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+            layers.BatchNormalization(),
+            layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+            layers.BatchNormalization(),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            layers.BatchNormalization(),
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            layers.BatchNormalization(),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+
+            layers.Flatten(),
+
+            layers.Dense(64, activation="relu"),
+            layers.BatchNormalization(),
+            layers.Dropout(0.5),
+
+            layers.Dense(config.NUM_CLASSES, activation="softmax"),
+        ],
+        name="batchnorm_cnn"
+    )
+
+    return model
