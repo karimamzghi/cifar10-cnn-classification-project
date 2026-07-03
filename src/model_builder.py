80f8d5d42b9061e90d0a374a67f372769e6747c1
karimamzghi <karima.mzoughi@zeynlo.com>
2026-07-03 05:52:42 +0200

    implement adam optimizer model

---
+++ src/model_builder.py
@@ -0,0 +1,33 @@
+# Create Reusable CNN model architectures
+
+from pyexpat import model
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from src import config
+
+def build_dropout_cnn():
+    model = keras.Sequential(
+        [
+            keras.Input(shape=config.INPUT_SHAPE),
+
+            layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+            layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+
+            layers.Flatten(),
+
+            layers.Dense(64, activation="relu"),
+            layers.Dropout(0.5),
+
+            layers.Dense(config.NUM_CLASSES, activation="softmax"),
+        ],
+        name="dropout_cnn"
+    )
+
+    return model
